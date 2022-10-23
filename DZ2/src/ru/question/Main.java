cc3222342869c2309c891e45626a1112649a9b70
vbrovak <v.brovak@sigma-it.ru>
2022-10-23 17:21:23 +0300

    DZ_2

--- DZ2/src/ru/question/Main.java
+++ DZ2/src/ru/question/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
+    public static final int ERROR_INPUT_INT = -1000000;
     public static void main(String[] args) {
 
         int errcnt = 0; // количество ошибочных
@@ -13,45 +13,64 @@ public class Main {
         //массив вопросов
         String[] questions = {"Два конца, два кольца,посредине гвоздик",
                 "Не огонь, а жжется",
-                "Без рук, без топоренка, построена избенка"};
+                "Без рук, без топоренка, построена избенка",
+                "На колесах едет ловко, если тянешь за веревку"};
         //массив номеров правильных ответов
-        int[] corrects = {2, 1, 3};
+        int[] corrects = {2, 1, 3, 1, 4};
 
         //двухмерный массив ответов
         String[][] answers = {
                 {"Молоток", "Ножницы", "Циркуль"},
                 {"Крапива", "Солнце", "Водка"},
-                {"Дом", "Коробка", "Гнездо"}
+                {"Дом", "Коробка", "Гнездо"},
+                {"Колесо", "Корабль", "Самолет", "Машинка"}
         };
 
-        Scanner scanner = new Scanner(System.in);
+
         //печать заголовка
         System.out.println(" *************************************************************************");
-        System.out.println("   Ответьте на 3 загадки. Надо ввести номер правильного ответа (1-3)      ");
+        System.out.println("  Ответьте на 4 загадки. Надо ввести НОМЕР правильного ответа.0 - прервать ");
         System.out.println(" *************************************************************************");
 
         // == цикл по вопросам ==
-        for (int i = 1; i < 4; i++) {
+        for (int i = 1; questions.length >= i; i++) {
             System.out.println("Вопрос N" + i + ": " + questions[i - 1]);
             //печать вариантов
-            for (int j = 1; j < 4; j++) {
+            for (int j = 1; answers[i-1].length >= j; j++) {
                 System.out.println(j + "." + answers[i - 1][j - 1]);
             }
-            //запрос ответа (по-хорошему надо обернуть эксепшеном, но еще не проходили)
-            int a = scanner.nextInt();
+            int a = ERROR_INPUT_INT;
+            do {
+                System.out.println("Введите ответ (1 - " + answers[i-1].length + "), 0 - прервать задачу");
+                Scanner sc = new Scanner(System.in);
+                if (sc.hasNextInt()) {
+                    a = sc.nextInt();
+                    if (a == 0) {
+                        System.out.println(" >> Работа программы прервана пользователем");
+                        return;
+                    }
+                    if (a < 0 || a > answers[i-1].length) {
+                        a = ERROR_INPUT_INT;
+                    }
+                    }
+                else {
+                    a = ERROR_INPUT_INT;
+                }
+            }
+                    while (a == ERROR_INPUT_INT) ;
 
-            if (a == corrects[i - 1]) {
-                System.out.println("Верно!");
-            } else {
-                // Про corrects[i-1]-1 Два раза -1 потому, что индексы массивов с 0 начинаются.
-                System.out.println("Неверно ! Правильный ответ: " + (corrects[i - 1]) + "." + answers[i - 1][corrects[i - 1] - 1]);
-                errcnt++;
-            } //конец обработки условного оператора
-            System.out.println();
-        }
-        System.out.println(" *************************************************************************");
-        System.out.println(" Ваши результаты - Правильных ответов:" + (3 - errcnt) + ", Неправильных:" + errcnt);
-        System.out.println(" *************************************************************************");
-
+        if (a == corrects[i - 1]) {
+            System.out.println("Верно!");
+        } else {
+            // Про corrects[i-1]-1 Два раза -1 потому, что индексы массивов с 0 начинаются.
+            System.out.println("Неверно ! Правильный ответ: " + (corrects[i - 1]) + "." + answers[i - 1][corrects[i - 1] - 1]);
+            errcnt++;
+        } //конец обработки условного оператора
+        System.out.println();
     }
+        System.out.println(" *************************************************************************");
+        System.out.println(" Ваши результаты - Правильных ответов:"+(questions.length-errcnt)+", Неправильных:"+errcnt);
+        System.out.println(" *************************************************************************");
+
+}
 }
