78c1892e8fe9f6c5062d5ac22d20526b1282f6f9
vbrovak <115388482+vbrovak@users.noreply.github.com>
2022-12-17 21:01:15 +0300

    DZ8 (#7)

---
+++ DZ8/test_data.sql
@@ -0,0 +1,43 @@
+-- Тестовые данные в таблицу вопросов
+
+insert into answers(id_answer,id_quest,vl_answer, pr_correct)
+values(1,1,'Ответ 1 к первому вопросу',0);
+
+insert into answers(id_answer,id_quest,vl_answer, pr_correct)
+values(2,1,'Ответ 2 к первому вопросу',1);
+
+insert into answers(id_answer,id_quest,vl_answer, pr_correct)
+values(3,2,'Ответ 1 ко второму вопросу',0);
+
+insert into answers(id_answer,id_quest,vl_answer, pr_correct)
+values(4,2,'Ответ 2 ко второму вопросу',1);
+
+insert into answers(id_answer,id_quest,vl_answer, pr_correct)
+values(5,2,'Ответ 3 ко второму вопросу',0);
+
+
+commit;
+
+-- Тестовые данные в таблицу ответов
+
+insert into answers(id_answer,id_quest,vl_answer, pr_correct)
+values(1,1,'Ответ 1 к первому вопросу',0);
+
+insert into answers(id_answer,id_quest,vl_answer, pr_correct)
+values(2,1,'Ответ 2 к первому вопросу',1);
+
+insert into answers(id_answer,id_quest,vl_answer, pr_correct)
+values(3,2,'Ответ 1 ко второму вопросу',0);
+
+insert into answers(id_answer,id_quest,vl_answer, pr_correct)
+values(4,2,'Ответ 2 ко второму вопросу',1);
+
+insert into answers(id_answer,id_quest,vl_answer, pr_correct)
+values(5,2,'Ответ 3 ко второму вопросу',0);
+
+
+commit;
+
+-- https://extendsclass.com/postgresql-online.htm
+-- https://www.db-fiddle.com/
+
